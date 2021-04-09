58401ea65ac8bd846c9734e140773e79c71bd950
John Sanabria <john.sanabria@correounivalle.edu.co>
2021-04-09 15:56:48 +0000

    fork exe finalizado

--- fork_exe_01.c
+++ fork_exe_01.c
@@ -1,6 +1,22 @@
 #include <stdio.h> // std - standard, io input/output
 #include <unistd.h>
+#include <sys/types.h>
+#include <assert.h>
 
 int main(int argc, char* argv[]){
+  int contador = 1;
+  pid_t pid;
 
+  pid = fork();
+
+  assert(pid >= 0);
+
+  if (pid == 0) {
+    contador = contador + 5;
+  } else {
+    contador += 2;
+  }
+  printf("%d\n",contador);
+
+  return 0;
 }
